3206227599294858560322ca66ba988ac118aefd
JeffTheK <jeff144@protonmail.com>
2021-05-23 18:05:27 +0300

    Fix rspec example

--- example/rspec_test.rb
+++ example/rspec_test.rb
@@ -1,7 +1,14 @@
-describe RspecExample do
+class TestClass
+  def two_plus_two
+    2 + 2
+  end
+end
+
+describe TestClass do
   context 'When adding 2 plus 2' do
     it 'Should be equal to 4' do
-      expect(2 + 2).to eq 4
+      cl = TestClass.new
+      expect(cl.two_plus_two).to eq 4
     end
   end
 end
